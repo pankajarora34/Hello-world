8a6d326ebc0b115d2726cf4c5e6e1ec5259c7226
Pankaj <arora_pankaj@hotmail.com>
2020-07-07 01:42:17 +0530

    updated Hello file

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -55,8 +55,7 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage("Pankaj How are you today") + "</h1>");
-		writer.println("<h1> Welcome to the Meeting </h1>"); 
+        writer.println("<h1>" + helloService.createHelloMessage("PA How are you") + "</h1>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
