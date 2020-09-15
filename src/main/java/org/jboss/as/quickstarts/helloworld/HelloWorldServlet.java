e95971c206d21ba3e0bcb25778249a9bc510b150
Pankaj <arora_pankaj@hotmail.com>
2020-09-15 18:26:01 +0530

    first commit

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -55,7 +55,7 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage("To All How are you!!") + "</h1>");
+        writer.println("<h1>" + helloService.createHelloMessage("To All How are you Welcome !!") + "</h1>");
         writer.println("<h1> Welcome to Session for Devops</h1>");
         writer.println("<h1> Welcome to Session for CI/CD</h1>");
         writer.println(PAGE_FOOTER);
